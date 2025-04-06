d3a57eb85e44d0c38a6b23ca6271320c115c7d97
Jairam B <b.jairam0512@gmail.com>
2025-04-06 13:42:09 +0530

    fixing routes for non github pages deployment

---
+++ src/router/routes.tsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter, Outlet } from "react-router-dom";
+// import App from "../App";
+import AboutComponent from "../pages/AboutComponent";
+import ContactComponent from "../pages/ContactComponent";
+import MainComponent from "../pages/MainComponent";
+import FooterComponent from "../components/FooterComponent";
+import HeaderComponent from "../components/HeaderComponent";
+
+const router = createBrowserRouter([
+    {
+        // path: "/",
+        // element: <App />,
+        path: "/",
+        element: (
+            <>
+                <HeaderComponent />
+                <Outlet />
+                <FooterComponent />
+            </>
+        ),
+        children: [
+            {
+                path: "/",
+                element: <MainComponent />,
+            },
+            {
+                path: "about",
+                element: <AboutComponent />,
+            },
+            {
+                path: "contact",
+                element: <ContactComponent />,
+            },
+        ],
+    },
+]);
+
+export default router;
